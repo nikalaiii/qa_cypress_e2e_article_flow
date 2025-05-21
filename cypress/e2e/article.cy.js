bf389b6bf041352e958ec69e3f4818e14c443220
Mykola Stesiuk <mikalaj.krutak@gmail.com>
2025-05-21 16:21:15 +0200

    final code

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,13 +1,59 @@
-describe('', () => {
-  before(() => {
+import { faker } from '@faker-js/faker';
 
+describe('page', () => {
+  beforeEach(() => {
+    cy.visit('https://conduit.mate.academy/');
   });
 
-  it('', () => {
+  it('should provide an ability to crate post after login', () => {
+    const userName = faker.person.lastName();
+    const userMail = faker.internet.email();
+    cy.register(userMail, userName, '321123');
 
+    cy.get(':nth-child(2) > .nav-link').click();
+
+    cy.get(':nth-child(4) > .nav-link').click();
+
+    cy.url().should('include', userName.toLowerCase());
+
+    cy.createArticle(
+      faker.lorem.words(3),
+      faker.lorem.sentence(),
+      faker.lorem.paragraph(10)
+    );
+
+    cy.reload();
+    cy.get('.article-meta > :nth-child(1) > .hide-text').should('exist');
   });
 
-  it('', () => {
+  it('shoyld provide an ability to delete post after login', () => {
+    const userName = faker.person.lastName();
+    const userMail = faker.internet.email();
+    cy.register(userMail, userName, '321123');
 
+    cy.reload();
+
+    cy.createArticle(
+      faker.lorem.words(3),
+      faker.lorem.sentence(),
+      faker.lorem.paragraph(10)
+    );
+
+    cy.reload();
+
+    cy.get(':nth-child(4) > .nav-link').click();
+
+    cy.get('.article-meta > :nth-child(1) > .hide-text').should('exist');
+
+    cy.get('.preview-link > span').click();
+
+    cy.get(
+      '.article-actions > .article-meta > :nth-child(3) > .btn-outline-danger'
+    ).click();
+
+    cy.get('.article-preview').should(
+      'contain.text',
+      'No articles are here... yet.'
+    );
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -26,7 +26,7 @@
 
 const imgUrl = 'https://static.productionready.io/images/smiley-cyrus.jpg';
 
-Cypress.Commands.add('login', (email, username, password) => {
+Cypress.Commands.add('register', (email, username, password) => {
   cy.request('POST', '/api/users', {
     user: {
       email,
